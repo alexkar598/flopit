4da03df76f307d30a9fa7149108b125601daf978
Aurélie M <50751742+radex02@users.noreply.github.com>
2024-02-07 22:54:26 -0500

    Installation et configuration d'un client GraphQL sur le client + docker

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,10 +1,18 @@
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {NbThemeModule} from "@nebular/theme";
+import {NbThemeModule} from '@nebular/theme';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {graphqlProvider} from './graphql.provider';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(NbThemeModule.forRoot())]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(NbThemeModule.forRoot()),
+    provideHttpClient(withFetch()),
+    graphqlProvider,
+  ],
 };
---
+++ client/src/app/graphql.provider.ts
@@ -0,0 +1,52 @@
+import {Apollo, APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import {
+  ApplicationConfig,
+  inject,
+  InjectionToken,
+  makeStateKey,
+  TransferState,
+} from '@angular/core';
+import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
+
+const uri = '/graphql';
+
+const APOLLO_CACHE = new InjectionToken<InMemoryCache>('apollo-cache');
+const STATE_KEY = makeStateKey<any>('apollo.state');
+
+export function apolloOptionsFactory(
+  httpLink: HttpLink,
+  cache: InMemoryCache,
+  transferState: TransferState,
+): ApolloClientOptions<any> {
+  const isBrowser = transferState.hasKey<any>(STATE_KEY);
+
+  if (isBrowser) {
+    const state = transferState.get<any>(STATE_KEY, null);
+    cache.restore(state);
+  } else {
+    transferState.onSerialize(STATE_KEY, () => {
+      return cache.extract();
+    });
+    // Reset cache after extraction to avoid sharing between requests
+    cache.reset();
+  }
+
+  return {
+    link: httpLink.create({uri}),
+    cache,
+  };
+}
+
+export const graphqlProvider: ApplicationConfig['providers'] = [
+  Apollo,
+  {
+    provide: APOLLO_CACHE,
+    useValue: new InMemoryCache(),
+  },
+  {
+    provide: APOLLO_OPTIONS,
+    useFactory: apolloOptionsFactory,
+    deps: [HttpLink, APOLLO_CACHE, TransferState],
+  },
+];
