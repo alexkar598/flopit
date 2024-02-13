c495103c171615aa373aea0a90e3096f7fd4123c
fleprohon <felix.leprohon@gmail.com>
2024-02-13 18:01:07 -0500

    fini layout page de connexion

--- client/src/app/pages/connexion/connexion.component.ts
+++ client/src/app/pages/connexion/connexion.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import {NbButtonModule, NbCardModule, NbInputModule} from "@nebular/theme";
+import {NbButtonModule, NbCardModule, NbFormFieldModule, NbIconModule, NbInputModule} from "@nebular/theme";
 
 @Component({
   standalone: true,
-  imports: [NbButtonModule, NbCardModule, NbInputModule],
+  imports: [NbButtonModule, NbCardModule, NbInputModule, NbFormFieldModule, NbIconModule],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.scss'
 })
 export class ConnexionComponent {
+  showPassword = false;
 
+  getInputType() {
+    if (this.showPassword) {
+      return 'text';
+    }
+    return 'password';
+  }
+
+  toggleShowPassword() {
+    this.showPassword = !this.showPassword;
+  }
 }
