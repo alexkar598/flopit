2e6b401f7cb5d56fbf4da1f347dfe215d75bbe0d
fleprohon <felix.leprohon@gmail.com>
2024-02-13 20:21:54 -0500

    changé form en ng-form

--- client/src/app/pages/connexion/connexion.component.ts
+++ client/src/app/pages/connexion/connexion.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import {NbButtonModule, NbCardModule, NbFormFieldModule, NbIconModule, NbInputModule} from "@nebular/theme";
 
 @Component({
   standalone: true,
-  imports: [NbButtonModule, NbCardModule, NbInputModule, NbFormFieldModule, NbIconModule, NbEvaIconsModule],
+  imports: [NbButtonModule, NbCardModule, NbInputModule, NbFormFieldModule, NbIconModule, NbEvaIconsModule, FormsModule],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.scss'
 })
