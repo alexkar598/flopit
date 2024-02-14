bb8b85e7c5420dbcd5fd5e040150800bb85938d8
fleprohon <felix.leprohon@gmail.com>
2024-02-14 01:17:26 -0500

    ajouté recherche, layout pas bon encore

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { NbButtonModule } from '@nebular/theme';
+import { FormsModule } from '@angular/forms';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbButtonModule, NbFormFieldModule, NbIconModule, NbInputModule } from '@nebular/theme';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [CommonModule, NbButtonModule],
+    imports: [CommonModule, NbButtonModule, NbFormFieldModule, NbIconModule, NbEvaIconsModule, FormsModule, NbInputModule],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
