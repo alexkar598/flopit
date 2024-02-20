d9e70ac790963cee39ede44fe4acc2560f24b1fd
Aurélie M <50751742+radex02@users.noreply.github.com>
2024-02-20 13:04:27 -0500

    fix typo

--- client/src/app/pipes/relative-date.pipe.ts
+++ client/src/app/pipes/relative-date.pipe.ts
@@ -17,22 +17,22 @@ export class RelativeDatePipe implements PipeTransform {
 
     const daysSince = (now.getTime() - date.getTime()) / 86400000;
     if (Math.abs(daysSince) >= 7) {
-      const formater = new Intl.DateTimeFormat(language, {dateStyle: style, timeZone});
-      return formater.format(date);
+      const formatter = new Intl.DateTimeFormat(language, {dateStyle: style, timeZone});
+      return formatter.format(date);
     }
 
-    const formater = new Intl.RelativeTimeFormat(language, {style});
+    const formatter = new Intl.RelativeTimeFormat(language, {style});
 
-    if (Math.abs(daysSince) > 1) return formater.format(-Math.round(daysSince), 'days');
+    if (Math.abs(daysSince) > 1) return formatter.format(-Math.round(daysSince), 'days');
 
     const hoursSince = daysSince * 24
-    if (Math.abs(hoursSince) > 1) return formater.format(-Math.round(hoursSince), 'hours');
+    if (Math.abs(hoursSince) > 1) return formatter.format(-Math.round(hoursSince), 'hours');
 
     const minutesSince = hoursSince * 60
-    if (Math.abs(minutesSince) > 1) return formater.format(-Math.round(minutesSince), 'minutes');
+    if (Math.abs(minutesSince) > 1) return formatter.format(-Math.round(minutesSince), 'minutes');
 
     const secondsSince = minutesSince * 60
-    return formater.format(-Math.round(secondsSince), 'seconds');
+    return formatter.format(-Math.round(secondsSince), 'seconds');
   }
 
 }
