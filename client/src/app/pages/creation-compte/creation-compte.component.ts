a63dcc852e11e4a4e6392f75ab1b90043be0b937
JetEnd12 <118761269+JetEnd12@users.noreply.github.com>
2024-02-21 09:08:40 -0500

    ajouté les forms

--- client/src/app/pages/creation-compte/creation-compte.component.ts
+++ client/src/app/pages/creation-compte/creation-compte.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import {NbButtonModule, NbCardModule, NbFormFieldModule, NbIconModule, NbInputModule} from "@nebular/theme";
+
 
 @Component({
   selector: 'app-creation-compte',
   standalone: true,
-  imports: [],
+  imports: [NbButtonModule, NbCardModule, NbInputModule, NbFormFieldModule, NbIconModule, NbEvaIconsModule, FormsModule],
   templateUrl: './creation-compte.component.html',
   styleUrl: './creation-compte.component.scss'
 })
 export class CreationCompteComponent {
+  
+  showPassword = false;
 
+  getInputType() {
+    if (this.showPassword) {
+      return 'text';
+    }
+    return 'password';
+  }
+
+  toggleShowPassword() {
+    this.showPassword = !this.showPassword;
+  }
 }
