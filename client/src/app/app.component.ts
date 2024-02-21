a5519932eb2fa153b2e2393724b7a2958624a5ed
fleprohon <felix.leprohon@gmail.com>
2024-02-21 09:15:40 -0500

    Ajouté textes pour utilisateur et commentaire, icone utilisateur, compteur de votes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {RouterOutlet} from "@angular/router";
 import {NbButtonModule, NbLayoutModule} from "@nebular/theme";
+import { CommentaireComponent } from "./components/commentaire/commentaire.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NbLayoutModule, NbButtonModule],
+  imports: [CommonModule, RouterOutlet, NbLayoutModule, NbButtonModule, CommentaireComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
--- client/src/app/components/commentaire/commentaire.component.ts
+++ client/src/app/components/commentaire/commentaire.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbButtonModule, NbCardModule, NbIconModule } from '@nebular/theme';
 
 @Component({
   selector: 'app-commentaire',
   standalone: true,
-  imports: [],
+  imports: [NbCardModule, FormsModule, NbIconModule, NbEvaIconsModule, NbButtonModule],
   templateUrl: './commentaire.component.html',
   styleUrl: './commentaire.component.scss'
 })
