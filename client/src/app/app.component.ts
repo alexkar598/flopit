668472279dfcf078b6fb434450d18cbf36d34bd9
fleprohon <felix.leprohon@gmail.com>
2024-02-21 11:31:42 -0500

    enlevé composant de page principale

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,12 +2,11 @@ import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {RouterOutlet} from "@angular/router";
 import {NbButtonModule, NbLayoutModule} from "@nebular/theme";
-import { CommentaireComponent } from "./components/commentaire/commentaire.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NbLayoutModule, CommentaireComponent],
+  imports: [CommonModule, RouterOutlet, NbLayoutModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
