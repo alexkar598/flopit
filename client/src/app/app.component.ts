ce956c87ee8535e6757e1b2021db4b6a8398b929
JetEnd12 <118761269+JetEnd12@users.noreply.github.com>
2024-02-21 03:07:47 -0500

    creation de la page CreationCompte

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,13 +2,14 @@ import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {RouterOutlet} from "@angular/router";
 import {NbButtonModule, NbLayoutModule} from "@nebular/theme";
+import { CreationCompteComponent } from "./pages/creation-compte/creation-compte.component";
 
 @Component({
-  selector: "app-root",
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, NbLayoutModule, NbButtonModule],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
+    selector: "app-root",
+    standalone: true,
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.scss",
+    imports: [CommonModule, RouterOutlet, NbLayoutModule, NbButtonModule, CreationCompteComponent]
 })
 export class AppComponent {
 }
---
+++ client/src/app/pages/creation-compte/creation-compte.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creation-compte',
+  standalone: true,
+  imports: [],
+  templateUrl: './creation-compte.component.html',
+  styleUrl: './creation-compte.component.scss'
+})
+export class CreationCompteComponent {
+
+}
