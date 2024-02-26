60d63aaeb24e00c30274ab56da88da826637f8eb
fleprohon <felix.leprohon@gmail.com>
2024-02-26 11:43:49 -0500

    créé pâge sub, ajouté route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import {Routes} from "@angular/router";
 import { ConnexionComponent } from "./pages/connexion/connexion.component";
+import { SubComponent } from "./pages/sub/sub.component";
 
 export const routes: Routes = [
-    {path: 'connexion', component: ConnexionComponent}
+    {path: 'connexion', component: ConnexionComponent},
+    {path: 'sub', component: SubComponent}
 ];
