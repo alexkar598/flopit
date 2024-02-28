cca4520891a784a6fe4473286a319119cbd816a6
Aurélie M <50751742+radex02@users.noreply.github.com>
2024-02-28 08:09:20 -0500

    update du codegen quand un schéma change

--- client/poll-gql.sh
+++ client/poll-gql.sh
@@ -3,11 +3,11 @@
 ../node_modules/.bin/graphql-codegen
 
 # Date de dernière modification
-LTIME=$(find src -type f -name "*.graphql" -print0 | xargs -0 stat | sort -nr | head -1)
+LTIME=$(find /app -type f -name "*.graphql" -print0 | xargs -0 stat | sort -nr | head -1)
 
 while true
 do
-   ATIME=$(find src -type f -name "*.graphql" -print0 | xargs -0 stat | sort -nr | head -1)
+   ATIME=$(find /app -type f -name "*.graphql" -print0 | xargs -0 stat | sort -nr | head -1)
 
    if [[ "$ATIME" != "$LTIME" ]]
    then
