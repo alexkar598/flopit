5027db4be5ceec2908c46556d6e89a9026b10768
Alex K <25136265+alexkar598@users.noreply.github.com>
2024-03-04 18:57:48 -0500

    flopit-100 Ajout de sortable sur children

--- server/src/modules/post/basepost/connections/children.ts
+++ server/src/modules/post/basepost/connections/children.ts
@@ -1,10 +1,27 @@
 import { builder } from "../../../../builder.ts";
 import { commentRef } from "../../comment/schema.ts";
+import {
+  extractQueryOptions,
+  postSortOptionsRef,
+  PostSortType,
+} from "../../sortable.ts";
 
 builder.prismaInterfaceField("Post", "children", (t) =>
   t.relatedConnection("Children", {
     cursor: "id",
     totalCount: true,
     type: commentRef,
+    args: {
+      sortOptions: t.arg({ type: postSortOptionsRef, required: false }),
+    },
+    query: ({ sortOptions }) => {
+      const querySortOptions = extractQueryOptions(
+        sortOptions,
+        PostSortType.Top,
+      );
+      return {
+        ...querySortOptions,
+      };
+    },
   }),
 );
