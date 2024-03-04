3df01d9fa7ed88ffaaa3420f9706e89ddc4bce03
Alex K <25136265+alexkar598@users.noreply.github.com>
2024-03-03 22:01:30 -0500

    flopit-94 Mutation CreateUser

---
+++ server/src/modules/user/mutations/createUser.ts
@@ -0,0 +1,51 @@
+import { builder } from "../../../builder.ts";
+import { prisma } from "../../../db.ts";
+import crypto from "crypto";
+import { getAPIError } from "../../../util.ts";
+import { compute_hash } from "../../auth/auth.ts";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
+const input = builder.inputType("CreateUserInput", {
+  fields: (t) => ({
+    email: t.string(),
+    username: t.string(),
+    password: t.string(),
+  }),
+});
+
+builder.mutationField("createUser", (t) =>
+  t.prismaField({
+    type: "User",
+    nullable: true,
+    args: { input: t.arg({ type: input }) },
+    resolve: async (
+      query,
+      _root,
+      { input: { email, username, password: cleartext_password } },
+    ) => {
+      if (cleartext_password.length < 6) throw getAPIError("TRIVIAL_PASSWORD");
+
+      const salt = crypto.randomBytes(32);
+      const password = await compute_hash(salt, cleartext_password);
+
+      try {
+        return await prisma.user.create({
+          ...query,
+          data: {
+            salt,
+            username,
+            email,
+            password,
+          },
+        });
+      } catch (e) {
+        if (e instanceof PrismaClientKnownRequestError && e.code === "P2002") {
+          if (e.meta?.target === "User_username_key")
+            throw getAPIError("DUPLICATE_USERNAME");
+          if (e.meta?.target === "User_email_key")
+            throw getAPIError("DUPLICATE_EMAIL");
+        }
+      }
+    },
+  }),
+);
--- shared/apierror.ts
+++ shared/apierror.ts
@@ -3,4 +3,7 @@ export const APIError = Object.freeze({
   NOT_IMPLEMENTED: "Cette opération n'est pas encore implémentée",
   INVALID_OID: "Un OID fournit est invalide",
   BAD_CREDENTIALS: "Courriel ou mot de passe invalide",
+  DUPLICATE_USERNAME: "Ce nom d'utilisateur est déja utilisé",
+  DUPLICATE_EMAIL: "Ce courriel est déja utilisé",
+  TRIVIAL_PASSWORD: "Le mot de passe doit avoir 6 caractères",
 });
