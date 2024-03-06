0cec64a59435433ace751d6a20f0a24b2bf92ee6
JetEnd12 <118761269+JetEnd12@users.noreply.github.com>
2024-03-06 00:02:41 -0500

    supprimé ""@schematics/angular": "^17.1.3", et utilisé nblayoutfooter

--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NbLayoutModule } from '@nebular/theme';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NbLayoutModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
