d6ce784bfb713f55f59d3b50cc1b0346b6604d88
Alex K <25136265+alexkar598@users.noreply.github.com>
2024-03-06 20:53:44 -0500

    Prettier

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,20 @@
-import {Component} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {RouterOutlet} from "@angular/router";
-import {NbButtonModule, NbLayoutModule} from "@nebular/theme";
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { NbButtonModule, NbLayoutModule } from "@nebular/theme";
 import { ListeDePublicationsComponent } from "./composants/liste-de-publications/liste-de-publications.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NbLayoutModule, NbButtonModule, ListeDePublicationsComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NbLayoutModule,
+    NbButtonModule,
+    ListeDePublicationsComponent,
+  ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-}
+export class AppComponent {}
--- client/src/app/composants/liste-de-publications/liste-de-publications.component.ts
+++ client/src/app/composants/liste-de-publications/liste-de-publications.component.ts
@@ -1,13 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { Component} from '@angular/core';
-import {NbCardModule, NbListModule, } from '@nebular/theme';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { NbCardModule, NbListModule } from "@nebular/theme";
 
 @Component({
-  selector: 'app-liste-de-publications',
+  selector: "app-liste-de-publications",
   standalone: true,
   imports: [NbListModule, NbCardModule, CommonModule],
-  templateUrl: './liste-de-publications.component.html',
-  styleUrl: './liste-de-publications.component.scss'
+  templateUrl: "./liste-de-publications.component.html",
+  styleUrl: "./liste-de-publications.component.scss",
 })
 export class ListeDePublicationsComponent {
   articles: any[] = [];
@@ -16,9 +16,9 @@ export class ListeDePublicationsComponent {
     // Use a for loop to generate values up to 20
     for (let i = 1; i <= 20; i++) {
       this.articles.push({
-        user: 'wdfwf',
+        user: "wdfwf",
         title: `Article ${i}`,
-        content: `This is the content of Article ${i}.`
+        content: `This is the content of Article ${i}.`,
       });
     }
   }
