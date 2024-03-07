06a968b66dc91949b5b8d7d7893dee7375649aa7
Alex K <25136265+alexkar598@users.noreply.github.com>
2024-03-07 00:16:06 -0500

    flopit-50 Retrait d'un import inutile

--- client/src/app/components/top-post-list/top-post-list.component.ts
+++ client/src/app/components/top-post-list/top-post-list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { NbCardModule, NbListModule } from "@nebular/theme";
 import { Observable } from "rxjs";
 import { TopPostCardFragment } from "~/graphql";
