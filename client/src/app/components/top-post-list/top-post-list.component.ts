237130bb91ca3256dee0f2a3560bfae3add02f76
Alex K <25136265+alexkar598@users.noreply.github.com>
2024-03-07 00:15:16 -0500

    flopit-50 Composante top post list

---
+++ client/src/app/components/top-post-list/top-post-list.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from "@angular/common";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { NbCardModule, NbListModule } from "@nebular/theme";
+import { Observable } from "rxjs";
+import { TopPostCardFragment } from "~/graphql";
+
+@Component({
+  selector: "app-post-list",
+  standalone: true,
+  imports: [NbListModule, NbCardModule, CommonModule],
+  templateUrl: "./top-post-list.component.html",
+  styleUrl: "./top-post-list.component.scss",
+})
+export class TopPostListComponent {
+  @Output()
+  loadNext = new EventEmitter<void>();
+  @Input({ required: true })
+  posts$!: Observable<TopPostCardFragment[]>;
+}
