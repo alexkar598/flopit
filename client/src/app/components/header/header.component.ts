54cb7daf13c5a109ce1b54474acfe1bb910d4890
Aurélie M <50751742+radex02@users.noreply.github.com>
2024-03-08 18:46:41 -0500

    Bouton de déconnexion

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,17 +1,21 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterLink } from "@angular/router";
 import { NbEvaIconsModule } from "@nebular/eva-icons";
 import {
   NbButtonModule,
+  NbContextMenuModule,
   NbFormFieldModule,
   NbIconModule,
   NbInputModule,
+  NbMenuItem,
+  NbMenuService,
   NbUserModule,
 } from "@nebular/theme";
 import { UserService } from "~/app/services/user.service";
 import { GetImgPipe } from "~/app/pipes/get-img.pipe";
+import { filter } from "rxjs";
 
 @Component({
   selector: "app-header",
@@ -27,12 +31,34 @@ import { GetImgPipe } from "~/app/pipes/get-img.pipe";
     NbUserModule,
     GetImgPipe,
     RouterLink,
+    NbContextMenuModule,
   ],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.css",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+export class HeaderComponent implements OnInit {
+  constructor(
+    public userService: UserService,
+    private nbMenuService: NbMenuService,
+  ) {}
 
-export class HeaderComponent {
-  constructor(public userService: UserService) {}
+  public userMenuItems: NbMenuItem[] = [
+    {
+      title: "Log out",
+      icon: "log-out",
+      data: {
+        onClick: (async () => {
+          await this.userService.logout();
+        }).bind(this),
+      },
+    },
+  ];
+
+  ngOnInit() {
+    this.nbMenuService
+      .onItemClick()
+      .pipe(filter(({ tag }) => tag === "header-user-menu"))
+      .subscribe(({ item }) => item.data?.onClick());
+  }
 }
