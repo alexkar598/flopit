c8a7736da1f73ae2c860fc0f19b76edacd7ac7c7
Aurélie M <50751742+radex02@users.noreply.github.com>
2024-03-07 22:08:16 -0500

    Retirer un import de test

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from "@angular/router";
 import { CreationCompteComponent } from "~/app/pages/creation-compte/creation-compte.component";
 import { ConnexionComponent } from "./pages/connexion/connexion.component";
-import { TopPostListComponent } from "~/app/components/top-post-list/top-post-list.component";
 
 export const routes: Routes = [
   { path: "connexion", component: ConnexionComponent },
