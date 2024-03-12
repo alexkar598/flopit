513aac3b904fa0e1a28932a1173d0414176d6a73
Aurélie M <50751742+radex02@users.noreply.github.com>
2024-03-12 13:17:36 -0400

    Désactiver le bouton pour créer un post si non connecté

--- client/src/app/components/button-new-post/button-new-post.component.ts
+++ client/src/app/components/button-new-post/button-new-post.component.ts
@@ -1,16 +1,28 @@
 import { Component } from "@angular/core";
 import { CreatePostComponent } from "~/app/components/create-post/create-post.component";
-import { NbButtonModule, NbIconModule, NbWindowService } from "@nebular/theme";
+import {
+  NbButtonModule,
+  NbIconModule,
+  NbTooltipModule,
+  NbWindowService,
+} from "@nebular/theme";
+import { UserService } from "~/app/services/user.service";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
   selector: "app-button-new-post",
   standalone: true,
-  imports: [NbIconModule, NbButtonModule],
+  imports: [NbIconModule, NbButtonModule, AsyncPipe, NbTooltipModule],
   templateUrl: "./button-new-post.component.html",
   styleUrl: "./button-new-post.component.scss",
 })
 export class ButtonNewPostComponent {
-  constructor(private windowService: NbWindowService) {}
+  protected readonly Boolean = Boolean;
+
+  constructor(
+    private windowService: NbWindowService,
+    public userService: UserService,
+  ) {}
 
   ecrirePost() {
     this.windowService.open(CreatePostComponent, {
