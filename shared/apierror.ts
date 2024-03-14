84e5d1456c3005dfc715ff981a4b7943cec23567
Aurélie M <50751742+radex02@users.noreply.github.com>
2024-03-13 23:22:38 -0400

    Fix typos
    Co-authored-by: Felix2268157 <118761247+Felix2268157@users.noreply.github.com>

--- shared/apierror.ts
+++ shared/apierror.ts
@@ -11,7 +11,7 @@ export const APIError = Object.freeze({
   AUTHENTICATED_MUTATION: "Cette opération nécessite une session active",
   SUB_NOT_FOUND: "Ce f/ n'a pas été trouvé",
   POST_NOT_FOUND: "Ce message n'a pas été trouvé",
-  INVALID_DELTA: "Le contenue Delta n'est pas valide",
+  INVALID_DELTA: "Le contenu Delta n'est pas valide",
   TITLE_TOO_SHORT: "Le titre doit faire au moins 1 caractère",
-  BANNED: "Vous êtes bannie de ce sub",
+  BANNED: "Vous êtes banni de ce sub",
 });
