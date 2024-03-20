d96a5645cbff7a51b6c9aac6e870a17b201ab7ef
fleprohon <felix.leprohon@gmail.com>
2024-03-19 22:48:38 -0400

    tweaks layout, ajout input pour icone

--- client/src/app/pages/parametres/parametres.component.ts
+++ client/src/app/pages/parametres/parametres.component.ts
@@ -10,6 +10,9 @@ import {
   NbUserModule,
 } from "@nebular/theme";
 import { TopPostListComponent } from "~/app/components/top-post-list/top-post-list.component";
+import { GetImgPipe } from "~/app/pipes/get-img.pipe";
+import { AsyncPipe, NgOptimizedImage } from "@angular/common";
+import { UserService } from "~/app/services/user.service";
 
 @Component({
   selector: "app-parametres",
@@ -23,10 +26,16 @@ import { TopPostListComponent } from "~/app/components/top-post-list/top-post-li
     TopPostListComponent,
     NbSelectModule,
     NbTooltipModule,
+    GetImgPipe,
+    AsyncPipe,
+    NbUserModule,
+    NgOptimizedImage,
   ],
   templateUrl: "./parametres.component.html",
   styleUrl: "./parametres.component.scss",
 })
 export class ParametresComponent {
   selectedItem = "0";
+
+  constructor(public userService: UserService) {}
 }
