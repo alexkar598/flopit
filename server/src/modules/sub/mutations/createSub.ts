a4b2aa33d82664b3dd7982e0e93f464bdbf11881
Aurélie M <50751742+radex02@users.noreply.github.com>
2024-03-21 19:37:53 -0400

    Réduire le nombre de queries de création de sub

--- server/src/modules/sub/mutations/createSub.ts
+++ server/src/modules/sub/mutations/createSub.ts
@@ -15,7 +15,7 @@ builder.mutationField("createSub", (t) =>
     type: "Sub",
     nullable: true,
     args: { input: t.arg({ type: input }) },
-    resolve: (
+    resolve: async (
       query,
       _root,
       { input: { name, description } },
@@ -23,43 +23,38 @@ builder.mutationField("createSub", (t) =>
     ) => {
       if (!authenticated_user_id) throw getAPIError("AUTHENTICATED_MUTATION");
 
-      return prisma.$transaction(async (tx) => {
-        try {
-          const moderator = await tx.moderator.create({
-            select: { sub_id: true },
-            data: {
-              User: {
-                connect: {
-                  id: authenticated_user_id,
-                },
+      try {
+        const moderator = await prisma.moderator.create({
+          select: { Sub: { ...query } },
+          data: {
+            User: {
+              connect: {
+                id: authenticated_user_id,
               },
-              Sub: {
-                create: {
-                  name,
-                  description: description ?? undefined,
-                  Followers: {
-                    create: {
-                      user_id: authenticated_user_id,
-                    },
+            },
+            Sub: {
+              create: {
+                name,
+                description: description ?? undefined,
+                Followers: {
+                  create: {
+                    user_id: authenticated_user_id,
                   },
                 },
               },
             },
-          });
+          },
+        });
 
-          return await tx.sub.findUnique({
-            ...query,
-            where: { id: moderator.sub_id },
-          });
-        } catch (e) {
-          if (e instanceof PrismaClientKnownRequestError) {
-            //"Unique constraint failed on the {constraint}"
-            if (e.code === "P2002" && e.meta?.target === "Sub_name_key")
-              throw getAPIError("DUPLICATE_SUB_NAME");
-          }
-          throw e;
+        return moderator.Sub;
+      } catch (e) {
+        if (e instanceof PrismaClientKnownRequestError) {
+          //"Unique constraint failed on the {constraint}"
+          if (e.code === "P2002" && e.meta?.target === "Sub_name_key")
+            throw getAPIError("DUPLICATE_SUB_NAME");
         }
-      });
+        throw e;
+      }
     },
   }),
 );
