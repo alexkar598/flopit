6f8538410d840f15950149e5a06ec0f57c896ca4
fleprohon <felix.leprohon@gmail.com>
2024-04-03 10:50:23 -0400

    context menu encore

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 import { NbEvaIconsModule } from "@nebular/eva-icons";
 import {
   NbButtonModule,
@@ -40,9 +40,19 @@ export class HeaderComponent {
   constructor(
     public userService: UserService,
     public toastr: NbToastrService,
+    private router: Router,
   ) {}
 
   public userMenuItems: NbClickableMenuItem[] = [
+    {
+      title: "Créer une communauté",
+      icon: "globe-2-outline",
+      data: {
+        onClick: (async () => {
+          await this.router.navigate(["/f"]);
+        }).bind(this),
+      },
+    },
     {
       title: "Déconnexion",
       icon: "log-out",
