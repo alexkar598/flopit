5fc9714576d7dd4b0d52324c699b42ef740b2c4d
Alex K <25136265+alexkar598@users.noreply.github.com>
2024-04-29 14:33:49 -0400

    protection des sessions

--- server/src/modules/session/schema.ts
+++ server/src/modules/session/schema.ts
@@ -2,7 +2,10 @@ import { builder } from "../../builder.ts";
 
 export const sessionRef = builder.prismaNode("Session", {
   id: { field: "id" },
-  select: {},
+  select: {
+    user_id: true,
+  },
+  authScopes: ({ user_id }, ctx) => user_id === ctx.authenticated_user_id,
   fields: (t) => ({
     user: t.relation("User"),
     createdAt: t.expose("creation_time", {
