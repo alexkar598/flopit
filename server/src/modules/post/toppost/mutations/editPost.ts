417e85552c86495f10d610f7f128d7b6a411d0fc
Alex K <25136265+alexkar598@users.noreply.github.com>
2024-05-01 22:41:22 -0400

    mutation backend pour edit un post

---
+++ server/src/modules/post/toppost/mutations/editPost.ts
@@ -0,0 +1,59 @@
+import { builder } from "../../../../builder.ts";
+import { prisma } from "../../../../db.ts";
+import { deltaValidator } from "../../delta.ts";
+import { topPostRef, topPostValidators } from "../schema.ts";
+
+const input = builder.inputType("EditTopPostInput", {
+  fields: (t) => ({
+    id: t.globalID({ for: topPostRef }),
+    title: t.string({
+      validate: {
+        schema: topPostValidators.title,
+      },
+      required: false,
+    }),
+    delta_content: t.field({
+      type: "JSON",
+      validate: { schema: deltaValidator },
+      required: false,
+    }),
+  }),
+});
+
+builder.mutationField("editTopPost", (t) => {
+  return t.withAuth({ authenticated: true }).prismaField({
+    type: topPostRef,
+    nullable: true,
+    args: { input: t.arg({ type: input }) },
+    authScopes: async (_, args, { authenticated_user_id }) => {
+      const post = await prisma.post.findUniqueOrThrow({
+        select: {
+          Author: {
+            select: {
+              id: true,
+            },
+          },
+        },
+        where: {
+          id: args.input.id.id,
+        },
+      });
+
+      return post.Author?.id === authenticated_user_id;
+    },
+    resolve: async (query, _root, { input }) => {
+      return prisma.post.update({
+        ...query,
+        where: { id: input.id.id },
+        data: {
+          delta_content: input.delta_content ?? undefined,
+          TopPost: {
+            update: {
+              title: input.title ?? undefined,
+            },
+          },
+        },
+      });
+    },
+  });
+});
