f20bbb083996f48f8034a2100ed44f278f97e689
Alex K <25136265+alexkar598@users.noreply.github.com>
2024-05-18 04:11:19 -0400

    Refactor de sub info + meilleur upload d'images

--- client/src/app/pages/sub/sub.component.ts
+++ client/src/app/pages/sub/sub.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from "@angular/core";
+import { Component, DestroyRef, OnInit } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import {
   NbActionsModule,
@@ -16,10 +16,8 @@ import {
   EditSubGQL,
   FollowSubGQL,
   FollowSubMutation,
-  HomeFeedDocument,
-  SubFeedDocument,
+  SubInformationFragment,
   SubInformationGQL,
-  SubInformationQuery,
   UnfollowSubGQL,
   UnfollowSubMutation,
 } from "~/graphql";
@@ -31,9 +29,25 @@ import {
   switchMap,
   BehaviorSubject,
   tap,
+  filter,
+  combineLatestWith,
+  Observable,
+  Subject,
+  sample,
+  withLatestFrom,
+  finalize,
+  concat,
+  of,
+  shareReplay,
 } from "rxjs";
 import { TopPostListComponent } from "~/app/components/top-post-list/top-post-list.component";
-import { FormsModule } from "@angular/forms";
+import {
+  FormControl,
+  FormsModule,
+  NonNullableFormBuilder,
+} from "@angular/forms";
+import { FileInputAccessorModule } from "file-input-accessor";
+import { notNull } from "~/app/util";
 
 @Component({
   standalone: true,
@@ -48,150 +62,182 @@ import { FormsModule } from "@angular/forms";
     NbSpinnerModule,
     FormsModule,
     NbActionsModule,
+    FileInputAccessorModule,
   ],
   templateUrl: "./sub.component.html",
   styleUrl: "./sub.component.scss",
 })
-export class SubComponent {
-  public sub$ = new BehaviorSubject<SubInformationQuery["subByName"] | null>(
-    null,
-  );
+export class SubComponent implements OnInit {
+  protected actionToggleFollow$ = new Subject<void>();
+  protected actionEditModerators$ = new Subject<void>();
+  protected actionSave$ = new Subject<void>();
 
-  public editing: boolean = false;
+  public sub$!: Observable<SubInformationFragment>;
+
+  protected editing$ = new BehaviorSubject(false);
+
+  protected get editing() {
+    return this.editing$.getValue();
+  }
+
   private loading = false;
-  icon: File | null = null;
-  banner: File | null = null;
 
-  public newDescription: string = "";
+  protected form = this.formBuilder.group({
+    icon: new FormControl<File[] | null>(null),
+    banner: new FormControl<File[] | null>(null),
+    description: new FormControl(""),
+  });
+
+  protected iconUrl!: Observable<string | null>;
+  protected bannerUrl!: Observable<string | null>;
 
   constructor(
-    router: Router,
-    toastrService: NbToastrService,
+    private router: Router,
+    private toastrService: NbToastrService,
     protected windowService: NbWindowService,
     public route: ActivatedRoute,
     private subInfoQuery: SubInformationGQL,
     private followSubMut: FollowSubGQL,
     private unfollowSubMut: UnfollowSubGQL,
     private editSubMut: EditSubGQL,
-  ) {
-    this.route.paramMap
+    private formBuilder: NonNullableFormBuilder,
+    private destroyRef: DestroyRef,
+  ) {}
+
+  ngOnInit(): void {
+    this.sub$ = this.route.paramMap.pipe(
+      map((x) => x.get("subName")!),
+      distinctUntilChanged(),
+      switchMap(
+        (subName) =>
+          this.subInfoQuery.watch({
+            sub_name: subName,
+          }).valueChanges,
+      ),
+      map((res) => res.data.subByName),
+      tap((sub) => {
+        if (sub == null) {
+          this.toastrService.danger(
+            "Cette communauté n'a pas pu être trouvée",
+            "Communauté introuvée!",
+          );
+          void this.router.navigate(["/"]);
+        }
+      }),
+      filter(notNull),
+      takeUntilDestroyed(this.destroyRef),
+      shareReplay(1),
+    );
+
+    this.editing$
       .pipe(
-        map((x) => x.get("subName")!),
         distinctUntilChanged(),
-        switchMap(
-          (subName) =>
-            this.subInfoQuery.watch({
-              sub_name: subName,
-            }).valueChanges,
-        ),
-        map((res) => res.data.subByName),
-        tap((sub) => {
-          if (sub == null) {
-            toastrService.danger(
-              "Cette communauté n'a pas pu être trouvée",
-              "Communauté introuvée!",
-            );
-            void router.navigate(["/"]);
-          }
-        }),
-        takeUntilDestroyed(),
+        combineLatestWith(this.sub$),
+        map(([, sub]) => sub),
       )
-      .subscribe(this.sub$);
-  }
-
-  toggleFollow() {
-    const sub = this.sub$.getValue();
-    if (!sub) return;
-
-    const optimisticResponse: UnfollowSubMutation | FollowSubMutation = {
-      __typename: "Mutation",
-      [sub.isFollowing ? "unfollowSub" : "followSub"]: {
-        __typename: "Sub",
-        id: sub.id,
-        isFollowing: !sub.isFollowing,
-        followers: {
-          __typename: "SubFollowersConnection",
-          totalCount:
-            (sub.followers.totalCount ?? 0) + (sub.isFollowing ? -1 : 1),
-        },
-      },
-    };
-
-    if (sub.isFollowing)
-      this.unfollowSubMut
-        .mutate({ input: { subId: sub.id } }, { optimisticResponse })
-        .subscribe();
-    else
-      this.followSubMut
-        .mutate({ input: { subId: sub.id } }, { optimisticResponse })
-        .subscribe();
-
-    this.editing = false;
-  }
-
-  editDescription() {
-    const sub = this.sub$.getValue();
-    if (!sub) return;
-
-    this.editing = !this.editing;
-    if (this.editing) this.newDescription = sub.description;
-  }
-
-  saveDescription() {
-    const sub = this.sub$.getValue();
-    if (!sub) return;
-
-    this.editDescription();
-
-    this.loading = true;
-    this.editSubMut
-      .mutate(
-        {
-          input: {
-            id: sub.id,
-            banner: this.banner ?? undefined,
-            icon: this.icon ?? undefined,
-            description: this.newDescription,
-          },
-        },
-        {
-          optimisticResponse: {
-            editSub: {
-              ...sub,
-              description: this.newDescription,
-            },
-          },
-        },
-      )
-      .subscribe(async (res) => {
-        this.loading = false;
-        if (res.errors) return;
+      .subscribe((sub) => {
+        this.form.reset({
+          banner: null,
+          icon: null,
+          description: sub.description,
+        });
       });
-  }
 
-  openModeratorsWindow() {
-    const sub = this.sub$.getValue()?.id;
-    if (sub == null) return;
+    this.sub$.pipe(sample(this.actionToggleFollow$)).subscribe((sub) => {
+      const optimisticResponse: UnfollowSubMutation | FollowSubMutation = {
+        __typename: "Mutation",
+        [sub.isFollowing ? "unfollowSub" : "followSub"]: {
+          __typename: "Sub",
+          id: sub.id,
+          isFollowing: !sub.isFollowing,
+          followers: {
+            __typename: "SubFollowersConnection",
+            totalCount:
+              (sub.followers.totalCount ?? 0) + (sub.isFollowing ? -1 : 1),
+          },
+        },
+      };
 
-    this.windowService.open(ModeratorListWindowComponent, {
-      title: "Gérer modérateurs",
-      windowClass: "moderators-window",
-      closeOnEsc: false,
-      context: { sub },
+      if (sub.isFollowing)
+        this.unfollowSubMut
+          .mutate({ input: { subId: sub.id } }, { optimisticResponse })
+          .subscribe();
+      else
+        this.followSubMut
+          .mutate({ input: { subId: sub.id } }, { optimisticResponse })
+          .subscribe();
     });
-  }
 
-  onIconChange(input: any) {
-    const file = (input as HTMLInputElement)?.files?.[0];
-    if (!file) return;
+    this.sub$.pipe(sample(this.actionEditModerators$)).subscribe(({ id }) => {
+      this.windowService.open(ModeratorListWindowComponent, {
+        title: "Gérer modérateurs",
+        windowClass: "moderators-window",
+        closeOnEsc: false,
+        context: { sub: id },
+      });
+    });
 
-    this.icon = file;
-  }
+    this.form.valueChanges
+      .pipe(sample(this.actionSave$), withLatestFrom(this.sub$))
+      .subscribe(([formValues, sub]) => {
+        this.loading = true;
+        this.editSubMut
+          .mutate(
+            {
+              input: {
+                id: sub.id,
+                banner: formValues.banner?.[0] ?? undefined,
+                icon: formValues.icon?.[0] ?? undefined,
+                description: formValues.description ?? undefined,
+              },
+            },
+            {
+              optimisticResponse: {
+                editSub: {
+                  ...sub,
+                  description: formValues.description ?? sub.description,
+                },
+              },
+            },
+          )
+          .subscribe(async (res) => {
+            this.loading = false;
+            this.editing$.next(false);
+            if (res.errors) return;
+          });
+      });
 
-  onBannerChange(input: any) {
-    const file = (input as HTMLInputElement)?.files?.[0];
-    if (!file) return;
+    let lastIconPreviewUrl: string | null = null;
+    let lastBannerPreviewUrl: string | null = null;
+    this.iconUrl = concat(of(null), this.form.valueChanges).pipe(
+      map(() => this.form.getRawValue().icon),
+      withLatestFrom(this.sub$),
+      map(([x, sub]) => {
+        const file = x?.[0];
+        if (file == null) return sub.iconUrl ?? null;
 
-    this.banner = file;
+        if (lastIconPreviewUrl != null) URL.revokeObjectURL(lastIconPreviewUrl);
+        return (lastIconPreviewUrl = URL.createObjectURL(file));
+      }),
+      finalize(() => {
+        if (lastIconPreviewUrl != null) URL.revokeObjectURL(lastIconPreviewUrl);
+      }),
+    );
+    this.bannerUrl = concat(of(null), this.form.valueChanges).pipe(
+      map(() => this.form.getRawValue().banner),
+      withLatestFrom(this.sub$),
+      map(([x, sub]) => {
+        const file = x?.[0];
+        if (file == null) return sub.bannerUrl ?? null;
+
+        if (lastBannerPreviewUrl != null)
+          URL.revokeObjectURL(lastBannerPreviewUrl);
+        return (lastBannerPreviewUrl = URL.createObjectURL(file));
+      }),
+      finalize(() => {
+        if (lastBannerPreviewUrl != null)
+          URL.revokeObjectURL(lastBannerPreviewUrl);
+      }),
+    );
   }
 }
